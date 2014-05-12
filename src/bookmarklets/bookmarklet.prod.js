c57091bd135dfc89c835816440a4ad658dcd41b7
Yomguithereal <guillaumeplique@gmail.com>
2014-05-12 22:16:57 +0200

    Fix #33 - Dropping Math.random in prod bookmarklet

--- src/bookmarklets/bookmarklet.prod.js
+++ src/bookmarklets/bookmarklet.prod.js
@@ -8,8 +8,7 @@
    * HTTPS server (github for the time being).
    */
   var body = document.getElementsByTagName('body')[0],
-      script = document.createElement('script'),
-      r = Math.random();
+      script = document.createElement('script');
 
   script.src = '//raw.githubusercontent.com/medialab/artoo/master/build/artoo.min.js';
   script.type = 'text/javascript';

2cf9a1720f0510a425f22bff26450bdcedc60306
Yomguithereal <guillaumeplique@gmail.com>
2014-07-09 23:06:50 +0200

    Experiments

--- src/plugins/jquery.footprint.js
+++ src/plugins/jquery.footprint.js
@@ -10,58 +10,115 @@
    */
 
   // Helpers
-  function shallowArrayCompare(a, b) {
-    var i, l;
+  function dice(a, b) {
+    var intersection = 0,
+        al = a.length - 1,
+        bl = b.length - 1;
 
-    for (i = 0, l = Math.max(a.length, b.length); i < l; i++)
-      if (a[i] !== b[i])
+    if (al < 1 || bl < 1)
+      return 0;
+
+    var bis = [],
+        ai,
+        i,
+        j;
+
+    for (i = 0; i < bl; i++)
+      bis.push(b.slice(i, i + 2));
+
+    for (i = 0; i < al; i++) {
+      ai = a.slice(i, i + 2);
+
+      for (j = 0; j < bl; j++) {
+        if (ai[0] === bis[j][0] && ai[1] === bis[j][1]) {
+          intersection++;
+          bis[j] = [null, null];
+          break;
+        }
+      }
+    }
+
+    return (2.0 * intersection) / (al + bl);
+  }
+
+  function compare(a, b) {
+    if (Math.abs(a.length - b.length) > 1)
+      return false;
+
+    var index = {},
+        error = false,
+        k;
+
+    a.map(function(e) {
+      index[e] = true;
+    });
+
+    b.map(function(e) {
+      index[e] = true;
+    });
+
+    for (i in index) {
+      if ((!~a.indexOf(i) || !~b.indexOf(i)) && error) {
         return false;
+      }
+      else if (!~a.indexOf(i) || !~b.indexOf(i)) {
+        error = true;
+      }
+    }
+
     return true;
   }
 
-  // TODO: on content also
   function _footprint($) {
-    $.fn.footprint = function() {
+
+    // Plugin itself
+    $.fn.footprint = function(parent) {
+      parent = parent === undefined ? true : parent;
+
       var $e = $(this).first(),
           footprint = [],
-          identity,
           txt;
 
-      // Iterating through childrens
-      $e.children().each(function() {
+      // Tag name
+      footprint.push($e.prop('tagName').toLowerCase());
 
-        // Tag name
-        identity = $(this).prop('tagName').toLowerCase();
-
-        // Class if class is used more than once in document
-        $(this).classes().forEach(function(c) {
-          if ($('.' + c).length > 1 && c !== 'hidden' && c !== 'activated')
-            identity += '.' + c;
-        });
-
-        // What does each contains
-        var txt = $(this).text().trim();
-        if (!txt) {
-          identity += '@empty';
-        }
-        else {
-          if (~txt.search(/\w/))
-            identity += '@letters'
-          if (~txt.search(/\d/))
-            identity += '@numbers'
-        }
-
-        // Pushing identity
-        footprint.push(identity);
+      // Class if class is used more than once in document
+      $e.classes().forEach(function(c) {
+        if ($('.' + c).length > 1 && c !== 'hidden' && c !== 'activated')
+          footprint.push('.' + c);
       });
+
+      // What are the characteristics of the node
+      var txt = $e.text().trim(),
+          nb_words = (txt.match(/\S+/g) || []).length;
+
+      if ($e.children().length)
+        footprint.push('@children');
+      if (nb_words === 1)
+        footprint.push('@word')
+      if (nb_words > 1 && nb_words <= 3)
+        footprint.push('@words');
+      if (nb_words > 3)
+        footprint.push('@sentence');
+      if (~txt.search(/\w/))
+        footprint.push('@letters');
+      if (~txt.search(/\d/))
+        footprint.push('@numbers');
+
+      // Parent
+      // if (parent)
+      //   footprint.push(
+      //     '@parent(' + $e.parent().footprint(false).join(',') + ')'
+      //   );
 
       return footprint;
     };
 
-    $('*').click(function(e) {
-      console.log($(this).parent().footprint());
-      e.stopPropagation();
-      e.preventDefault();
+    // Extending selectors
+    $.extend($.expr[':'], {
+      footprint: function(e, i, f) {
+        return compare($(e).footprint(), f[3].split(','));
+      }
     });
   }
 
--- src/plugins/jquery.similar.js
+++ src/plugins/jquery.similar.js
@@ -12,11 +12,12 @@
   function _similar($) {
     $.fn.similar = function() {
       var $e = $(this).first(),
-          path = $e.path(false).split(' > '),
           $sel = null,
+          footprint = $e.footprint(),
+          path = $e.path(false).split(' > '),
           c,
           i;
-
+console.log(footprint);
       // We try to drop specific selection in a reverse order
       for (i = path.length - 1; i > -1; i--) {
         c = path[i];
@@ -28,23 +29,30 @@
 
         // Are there more than one element in the new selection?
         path[i] = c;
-        $sel = $(path.join(' > '));
+        $sel = $(path.join(' > ') + ':footprint(' + footprint.join(',') + ')');
 
-        if ($sel.length > 1)
+        // If two elements or more are selected, we have a potential winner
+        if ($sel.length > 2)
           break;
+
+        // TODO: try to compute identity from selected nodes and compute
+        // statistical anomalies.
+        // If anomalies overcome a threshold, the selector is probably not that good
+
+        // TODO: then we should try to expand to siblings just to be sure.
       }
 
       return $sel || $(this);
     };
 
-    // $('*').click(function(e) {
-    //   $('.aaa').css('color', 'black');
-    //   e.stopPropagation();
-    //   e.preventDefault();
-    //   $('.aaa').removeClass('aaa');
-    //   $(this).similar().addClass('aaa');
-    //   $('.aaa').css('color', 'red');
-    // });
+    $('*').click(function(e) {
+      $('.aaa').css('color', 'black');
+      e.stopPropagation();
+      e.preventDefault();
+      $('.aaa').removeClass('aaa');
+      $(this).similar().addClass('aaa');
+      $('.aaa').css('color', 'red');
+    });
   }
 
   // Exporting

3ad3bb803256b9049cf6dbaf4ec2d2802faa1096
Yomguithereal <guillaumeplique@gmail.com>
2014-09-28 21:26:12 +0200

    Adding helpers to cheerio

--- src/node/artoo.node.js
+++ src/node/artoo.node.js
@@ -12,8 +12,24 @@
   // Setting initial context
   artoo.$ = cheerio.load('');
 
+  // Applying helpers
+  var methods = ['scrape', 'scrapeOne', 'scrapeTable'];
+
+  methods.forEach(function(method) {
+
+    cheerio.prototype[method] = function() {
+      return artoo[method].apply(
+        artoo, [artoo.$(this)].concat(Array.prototype.slice.call(arguments)));
+    };
+  });
+
   // Methods
   artoo.setContext = function($) {
+
+    // Fixing context
     artoo.$ = $;
   };
 }).call(this);
+
+
+
